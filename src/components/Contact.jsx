2b80bfae8bf1164d5a34d2414c11e734e9ff2266
MikiShibabaw <119623992+amexyegondar@users.noreply.github.com>
2023-04-22 13:57:22 -0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
             ></textarea>
 
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Let's talk
+              Let's touch
             </button>
           </form>
         </div>
